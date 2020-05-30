e65566826624694e0b10d75ad55201549521a288
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-05-30 19:50:36 +0200

    GET fiches

--- app/controllers/fiche/ficheTechniqueController.js
+++ app/controllers/fiche/ficheTechniqueController.js
@@ -1,33 +1,40 @@
 const dbConn = require('../../db/pool');
 
-// Question : existe-t'il le concept de transaction pour envoyer plusieurs requêtes d'un coup comme dans Firestore ?
-
+// RECUPERE LA LISTE DES FICHES
 // TODO :
-// - Ajouter le paramètre optionnel id_utilisateur. Si présent, ne renvoyer que les fiches créées par cet utilisateur. Peut-être wrapper le tout dans un try catch ?
-// - Renvoyer le nom de la production en plus de l'id_production
-// - Renvoyer les id au format integer plutôt que string si possible (sans les guillemets autour)
-// - Ajouter attribut created (idéalement généré automatiquement lors du post)
-// - Ajouter attribut modified (idéalement géré lors du put)
-// - Renvoyer le fullname de l'utilisateur quand la table User sera implémentée (Enda)
+// - Asc : Renvoyer le nom de la production en plus de l'id_production
+// - Asc : Ajouter attribut created (Généré par postgre automatiquement lors du post ?)
+// - Asc : Ajouter attribut modified (Géré par postgre lors de chaque put ?)
+// - ENDA : Renvoyer le fullname de l'utilisateur quand la table User sera implémentée
 const getFiches = (request, response) => {
+  // Récupère le paramètre optionnel id_utilisateur pour filtrer les fiches techniques
+  const id_utilisateur = request.query.id_utilisateur || true;
+
+  // Construction de la requête pour récupérer la liste des fiches techniques
   const getFichesQuery =
-    'SELECT id,id_production,id_utilisateur,libelle,ini_debut,ini_fin,commentaire FROM fiche.fiche_technique ORDER BY id ASC';
-  dbConn.pool.query(getFichesQuery, (error, results) => {
+    'SELECT id,id_production,id_utilisateur,libelle FROM fiche.fiche_technique WHERE id_utilisateur=$1 ORDER BY id ASC';
+
+  // Envoi de la requête
+  dbConn.pool.query(getFichesQuery, [id_utilisateur], (error, results) => {
     if (error) {
       throw error;
     }
+
+    // Renvoi un array avec les fiches techniques
     response.status(200).send(results.rows);
   });
 };
 
-// A DISCUTER
-// Je t'ai fait une proposition. Pas mal de nested requests, c'est pas joli. A voir si on laisse comme ça, ou si on met du ES6 async/await.
-// J'ai pas du tout testé, il y a peut-être des coquilles.
+// CREER UNE NOUVELLE FICHE
+// TODO :
+// - Enda : Ajouter id_utilisateur dans la première requête SQL
 const postFiche = (request, response) => {
-  // Destructurer les données contenus dans la requête
+  // Je t'ai fait une proposition. Pas mal de nested requests. A voir si on laisse comme ça, ou si on met du ES6 async/await.
+  // J'ai pas testé, il y a peut-être des coquilles.
+
+  // Destructure les données contenus dans la requête
   const {
     libelle_fiche,
-    id_utilisateur,
     id_production,
     ini_debut,
     ini_fin,
@@ -37,12 +44,12 @@ const postFiche = (request, response) => {
 
   // Construction de la requête pour créer la fiche technique
   const postFicheQuery =
-    'INSERT INTO fiche.fiche_technique(id, libelle, id_utilisateur, id_production, ini_debut, ini_fin) VALUES (DEFAULT, $1, $2, $3, $4, $5)';
+    'INSERT INTO fiche.fiche_technique(id, libelle, id_production, ini_debut, ini_fin) VALUES (DEFAULT, $1, $2, $3, $4)';
 
   // Envoi de la requête
   dbConn.pool.query(
     postFicheQuery,
-    [libelle_fiche, id_utilisateur, id_production, ini_debut, ini_fin],
+    [libelle_fiche, id_production, ini_debut, ini_fin],
     (error, results) => {
       if (error) {
         throw error;
@@ -97,7 +104,7 @@ const postFiche = (request, response) => {
 
             // Ajoute les dépenses
             depenses.map(({ libelle_depense, montant }) => {
-              // Requête pour créer une dépense
+              // Construction de la requête pour créer une dépense
               const postDepenseQuery =
                 'INSERT INTO fiche.activite(id, id_activite, libelle_depense, montant) VALUES (DEFAULT, $1, $2, $3)';
 
@@ -125,9 +132,13 @@ const postFiche = (request, response) => {
   );
 };
 
-// CREER VUE
+// RECUPERE LE CONTENU D'UNE FICHE
+// TODO : Créer Vue
 const getFicheById = (request, response) => {
+  // Récupère l'id de la fiche technique depuis l'URL
   const id_fiche = request.params.id;
+
+  // Construction de la requête pour récupérer la fiche
   const getFicheByIdQuery = `WITH subquery AS(
     SELECT act.mois_relatif,SUM(d.montant) dep,act.id_fiche_technique,f.libelle FROM fiche.activite act LEFT JOIN fiche.depense d ON act.id=d.id_activite 
     JOIN fiche.fiche_technique f ON act.id_fiche_technique=f.id
@@ -135,6 +146,8 @@ const getFicheById = (request, response) => {
    )
    SELECT libelle,JSON_AGG(JSON_BUILD_OBJECT('mois relatif',mois_relatif,'depenses',dep)) flux FROM subquery WHERE subquery.id_fiche_technique=$1 
    GROUP BY libelle`;
+
+  // Envoi de la requête
   dbConn.pool.query(getFicheByIdQuery, [id_fiche], (error, results) => {
     if (error) {
       throw error;
@@ -143,6 +156,7 @@ const getFicheById = (request, response) => {
   });
 };
 
+// MODIFIER UNE FICHE
 // A DISCUTER
 const putFicheById = (request, response) => {
   const id_fiche = request.params.id;
