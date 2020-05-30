fff4610890a8d7f5acf4f6308116072edd369708
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-05-30 12:51:44 +0200

    Fixed port issue

--- server.js
+++ server.js
@@ -45,4 +45,4 @@ app.get('/analyses', analyseController.getAnalyses);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
