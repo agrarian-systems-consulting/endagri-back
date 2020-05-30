4dff2ee6cbbd70c45bfe569e749b268fdb79ea0d
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-05-30 19:12:07 +0200

    post fiche

--- app/controllers/fiche/ficheTechniqueController.js
+++ app/controllers/fiche/ficheTechniqueController.js
@@ -1,27 +1,126 @@
-const dbConn = require('../../db/pool')
+const dbConn = require('../../db/pool');
 
-// DONE
+// Question : existe-t'il le concept de transaction pour envoyer plusieurs requêtes d'un coup comme dans Firestore ?
+
+// TODO :
+// - Ajouter le paramètre optionnel id_utilisateur. Si présent, ne renvoyer que les fiches créées par cet utilisateur. Peut-être wrapper le tout dans un try catch ?
+// - Renvoyer le nom de la production en plus de l'id_production
+// - Renvoyer les id au format integer plutôt que string si possible (sans les guillemets autour)
+// - Ajouter attribut created (idéalement généré automatiquement lors du post)
+// - Ajouter attribut modified (idéalement géré lors du put)
+// - Renvoyer le fullname de l'utilisateur quand la table User sera implémentée (Enda)
 const getFiches = (request, response) => {
-    const getFichesQuery = 'SELECT id,id_production,id_utilisateur,libelle,ini_debut,ini_fin,commentaire FROM fiche.fiche_technique ORDER BY id ASC';
-    dbConn.pool.query(getFichesQuery, (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(results.rows)
-    })
-}
+  const getFichesQuery =
+    'SELECT id,id_production,id_utilisateur,libelle,ini_debut,ini_fin,commentaire FROM fiche.fiche_technique ORDER BY id ASC';
+  dbConn.pool.query(getFichesQuery, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(results.rows);
+  });
+};
 
 // A DISCUTER
+// Je t'ai fait une proposition. Pas mal de nested requests, c'est pas joli. A voir si on laisse comme ça, ou si on met du ES6 async/await.
+// J'ai pas du tout testé, il y a peut-être des coquilles.
 const postFiche = (request, response) => {
-  const { libelle_fiche, id_utilisateur } = request.body
-  const postFicheQuery = 'INSERT INTO fiche.fiche_technique(id, libelle, id_utilisateur) VALUES (DEFAULT, $1, $2)';
-  dbConn.pool.query(postFicheQuery, [libelle_fiche, id_utilisateur], (error, results) => {
-    if (error) {
-      throw error
+  // Destructurer les données contenus dans la requête
+  const {
+    libelle_fiche,
+    id_utilisateur,
+    id_production,
+    ini_debut,
+    ini_fin,
+    ventes,
+    activites,
+  } = request.body;
+
+  // Construction de la requête pour créer la fiche technique
+  const postFicheQuery =
+    'INSERT INTO fiche.fiche_technique(id, libelle, id_utilisateur, id_production, ini_debut, ini_fin) VALUES (DEFAULT, $1, $2, $3, $4, $5)';
+
+  // Envoi de la requête
+  dbConn.pool.query(
+    postFicheQuery,
+    [libelle_fiche, id_utilisateur, id_production, ini_debut, ini_fin],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+
+      console.log(`Création de la fiche technique : ${libelle_fiche}`);
+
+      // TODO Récupérer l'id_fiche_technique pour le mettre dans les activités
+
+      // Ajoute les ventes
+      ventes.map(
+        ({ id_marche, libelle_vente, quantite, mois_relatif, mois }) => {
+          // Construction de la requête pour créer une vente
+          const postVenteQuery =
+            'INSERT INTO fiche.vente(id, id_marche, libelle_vente, quantite, mois_relatif, mois) VALUES (DEFAULT, $1, $2, $3, $4, $5)';
+
+          // Envoi de la requête
+          dbConn.pool.query(
+            postActiviteQuery,
+            [id_fiche_technique, libelle_activite, mois_relatif, mois],
+            (error, results) => {
+              if (error) {
+                throw error;
+              }
+
+              console.log(`Ajout d'une vente ${libelle_vente}`);
+            }
+          );
+        }
+      );
+
+      // Ajoute les activités
+      activites.map(({ libelle_activite, mois_relatif, mois, depenses }) => {
+        // Construction de la requête pour créer une activité
+        const postActiviteQuery =
+          'INSERT INTO fiche.activite(id, id_fiche_technique, libelle, mois_relatif, mois) VALUES (DEFAULT, $1, $2, $3, $4)';
+
+        // Envoi de la requête
+        dbConn.pool.query(
+          postActiviteQuery,
+          [id_fiche_technique, libelle_activite, mois_relatif, mois],
+          (error, results) => {
+            if (error) {
+              throw error;
+            }
+
+            console.log(`Ajout d'une activité : ${libelle_activite}`);
+
+            // TODO : Récupérer l'id de l'activité pour attacher les dépenses
+
+            // Ajoute les dépenses
+            depenses.map(({ libelle_depense, montant }) => {
+              // Requête pour créer une dépense
+              const postDepenseQuery =
+                'INSERT INTO fiche.activite(id, id_activite, libelle_depense, montant) VALUES (DEFAULT, $1, $2, $3)';
+
+              // Envoi de la requête
+              dbConn.pool.query(
+                postDepenseQuery,
+                [id_activite, libelle_depense, montant],
+                (error, results) => {
+                  if (error) {
+                    throw error;
+                  }
+
+                  console.log(`Ajout d'une dépense : ${libelle_depense}`);
+                }
+              );
+            });
+          }
+        );
+      });
+
+      // Retourne l'id de la fiche technique pour rediriger l'application cliente vers la fiche technique qui vient d'être créée
+      // response.status(201).send(id_fiche_technique);
     }
-    response.status(201).send(`Create`)
-  })
-}
+  );
+};
 
 // CREER VUE
 const getFicheById = (request, response) => {
@@ -35,24 +134,29 @@ const getFicheById = (request, response) => {
    GROUP BY libelle`;
   dbConn.pool.query(getFicheByIdQuery, [id_fiche], (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
-    response.status(200).send(results.rows)
-  })
-}
+    response.status(200).send(results.rows);
+  });
+};
 
 // A DISCUTER
 const putFicheById = (request, response) => {
   const id_fiche = request.params.id;
   const { libelle_fiche } = request.body;
-  const putFicheByIdQuery = 'UPDATE fiche.fiche_technique SET libelle=$1 WHERE id=$2';
-  dbConn.pool.query(putFicheByIdQuery, [libelle_fiche,id_fiche], (error, results) => {
-    if (error) {
-      throw error
+  const putFicheByIdQuery =
+    'UPDATE fiche.fiche_technique SET libelle=$1 WHERE id=$2';
+  dbConn.pool.query(
+    putFicheByIdQuery,
+    [libelle_fiche, id_fiche],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`Update`);
     }
-    response.status(200).send(`Update`)
-  })
-}
+  );
+};
 
 // DONE
 const deleteFicheById = (request, response) => {
@@ -60,42 +164,52 @@ const deleteFicheById = (request, response) => {
   const deleteFicheByIdQuery = 'DELETE FROM fiche.fiche_technique WHERE id=$1';
   dbConn.pool.query(deleteFicheByIdQuery, [id_fiche], (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
-    response.status(200).send(`Delete`)
-  })
-}
+    response.status(200).send(`Delete`);
+  });
+};
 
 // CREER VUE
 const getFicheByIdFluxMensuels = (request, response) => {
   const id_fiche = request.params.id;
-  const getFicheByIdFluxMensuelsQuery = 'SELECT * FROM fiche.fiche_technique WHERE id=$1';
-  dbConn.pool.query(getFicheByIdFluxMensuelsQuery, [id_fiche], (error, results) => {
-    if (error) {
-      throw error
+  const getFicheByIdFluxMensuelsQuery =
+    'SELECT * FROM fiche.fiche_technique WHERE id=$1';
+  dbConn.pool.query(
+    getFicheByIdFluxMensuelsQuery,
+    [id_fiche],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(results.rows);
     }
-    response.status(200).send(results.rows)
-  })
-}
+  );
+};
 
 // CREER VUE
 const getFicheByIdFluxCategorie = (request, response) => {
   const id_fiche = request.params.id;
-  const getFicheByIdFluxCategorieQuery = 'SELECT * FROM fiche.fiche_technique WHERE id=$1';
-  dbConn.pool.query(getFicheByIdFluxCategorieQuery, [id_fiche], (error, results) => {
-    if (error) {
-      throw error
+  const getFicheByIdFluxCategorieQuery =
+    'SELECT * FROM fiche.fiche_technique WHERE id=$1';
+  dbConn.pool.query(
+    getFicheByIdFluxCategorieQuery,
+    [id_fiche],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(results.rows);
     }
-    response.status(200).send(results.rows)
-  })
-}
+  );
+};
 
 module.exports = {
-    getFiches,
-    postFiche,
-    getFicheById,
-    putFicheById,
-    deleteFicheById,
-    getFicheByIdFluxMensuels,
-    getFicheByIdFluxCategorie, 
-}
+  getFiches,
+  postFiche,
+  getFicheById,
+  putFicheById,
+  deleteFicheById,
+  getFicheByIdFluxMensuels,
+  getFicheByIdFluxCategorie,
+};
