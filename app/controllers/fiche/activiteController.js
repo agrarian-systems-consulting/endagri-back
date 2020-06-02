bd228ee4bf53a59248108b86916b88efc3376fc5
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-06-03 00:21:57 +0200

    Essais infructueux de Promise.all

--- app/controllers/fiche/activiteController.js
+++ app/controllers/fiche/activiteController.js
@@ -29,7 +29,7 @@ const postActivite = (request, response) => {
       console.log('id_activite = ' + id_activite);
 
       // Ajoute les dépenses
-      AsyncPutDepenses = () => {
+      AsyncPostDepenses = () => {
         // La création d'une Promise permet d'attendre que les dépenses soit ajoutées avant de récupérer l'activité avec ses dépenses associées
         // console.log('depenses = ' + JSON.stringify(depenses, true, 2));
 
@@ -62,26 +62,28 @@ const postActivite = (request, response) => {
       };
 
       // Permet d'attendre que les dépenses soit ajoutées avant de récupérer l'activité avec ses dépenses associées
-      AsyncPutDepenses().then(() => {
-        // Créé la requête pour récupérer l'activité avec toutes ses dépenses associées
-        const getActiviteAvecDepensesQuery = `SELECT a.*, json_agg(d.*) depenses FROM fiche.activite a LEFT JOIN fiche.depense d ON a.id = d.id_activite WHERE a.id=$1 GROUP BY a.id`;
+      AsyncPostDepenses()
+        .then(() => {
+          // Créé la requête pour récupérer l'activité avec toutes ses dépenses associées
+          const getActiviteAvecDepensesQuery = `SELECT a.*, json_agg(d.*) depenses FROM fiche.activite a LEFT JOIN fiche.depense d ON a.id = d.id_activite WHERE a.id=$1 GROUP BY a.id`;
 
-        // Envoi de la requête
-        dbConn.pool.query(
-          getActiviteAvecDepensesQuery,
-          [id_activite],
-          (error, results) => {
-            if (error) {
-              throw error;
+          // Envoi de la requête
+          dbConn.pool.query(
+            getActiviteAvecDepensesQuery,
+            [id_activite],
+            (error, results) => {
+              if (error) {
+                throw error;
+              }
+
+              // console.log('POST activite 3 : ' + JSON.stringify(results.rows, true, 2));
+
+              // Envoi de la réponse
+              response.status(201).send(results.rows[0]);
             }
-
-            // console.log('POST activite 3 : ' + JSON.stringify(results.rows, true, 2));
-
-            // Envoi de la réponse
-            response.status(201).send(results.rows[0]);
-          }
-        );
-      });
+          );
+        })
+        .catch(response.sendStatus(500));
     }
   );
 };
@@ -112,85 +114,69 @@ const putActivite = (request, response) => {
       // console.log('putActivite 1' + JSON.stringify(results.rows[0], true, 2));
 
       // Mettre à jour les dépenses
-      AsyncUpdateDepenses = () => {
-        return new Promise((successCallback, failureCallback) => {
-          //TODO : Il faudrait supprimer les dépenses qui ont été supprimés par l'application cliente.
 
-          if (depenses) {
-            // Boucle sur chacune des dépenses
-            depenses.map(({ id, libelle_depense, montant }) => {
-              // Si c'est une nouvelle dépense, la créer, sinon modifier l'existante
-              if (id === undefined) {
-                // Construction de la requête pour créer une dépense
-                const postDepenseQuery = `INSERT INTO fiche.depense(id, id_activite, libelle, montant) VALUES (DEFAULT, $1, $2, $3) RETURNING *`;
+      //TODO : Il faudrait supprimer les dépenses qui ont été supprimés par l'application cliente.
 
-                // Envoi de la requête
-                dbConn.pool.query(
-                  postDepenseQuery,
-                  [id_activite, libelle_depense, montant],
-                  (error, results) => {
-                    if (error) {
-                      failureCallback(
-                        'Problème lors de la création des dépenses'
-                      );
+      if (depenses) {
+        // Boucle sur chacune des dépenses
+        depenses.map(({ id, libelle_depense, montant }) => {
+          // Si c'est une nouvelle dépense, la créer, sinon modifier l'existante
+          if (id === undefined) {
+            // Construction de la requête pour créer une dépense
+            const postDepenseQuery = `INSERT INTO fiche.depense(id, id_activite, libelle, montant) VALUES (DEFAULT, $1, $2, $3) RETURNING *`;
 
-                      throw error;
-                    }
-                    // console.log('putActivite 2' + JSON.stringify(results.rows[0], true, 2));
-                  }
-                );
-              } else {
-                // Construction de la requête pour modifier une dépense existante
-                const putDepenseQuery = `UPDATE fiche.depense SET id_activite=$1, libelle=$2, montant=$3 WHERE id=$4 RETURNING *`;
-                // Envoi de la requête
-                dbConn.pool.query(
-                  putDepenseQuery,
-                  [id_activite, libelle_depense, montant, id],
-                  (error, results) => {
-                    if (error) {
-                      failureCallback(
-                        'Problème lors de la mise à jour des dépenses'
-                      );
-
-                      throw error;
-                    }
-
-                    // console.log(
-                    //   'putActivite 3' + JSON.stringify(results.rows[0], true, 2)
-                    // );
-                  }
-                );
+            // Envoi de la requête
+            dbConn.pool.query(
+              postDepenseQuery,
+              [id_activite, libelle_depense, montant],
+              (error, results) => {
+                if (error) {
+                  throw error;
+                }
+                // console.log('putActivite 2' + JSON.stringify(results.rows[0], true, 2));
               }
-            });
+            );
           } else {
-            successCallback('Pas de dépenses associées');
+            // Construction de la requête pour modifier une dépense existante
+            const putDepenseQuery = `UPDATE fiche.depense SET id_activite=$1, libelle=$2, montant=$3 WHERE id=$4 RETURNING *`;
+            // Envoi de la requête
+            dbConn.pool.query(
+              putDepenseQuery,
+              [id_activite, libelle_depense, montant, id],
+              (error, results) => {
+                if (error) {
+                  throw error;
+                }
+
+                // console.log(
+                //   'putActivite 3' + JSON.stringify(results.rows[0], true, 2)
+                // );
+              }
+            );
           }
         });
-      };
+      }
 
-      // Permet d'attendre que les dépenses soit créées, modifiées ou supprimées avant de récupérer l'activité et ses dépenses associées
-      AsyncUpdateDepenses().then(() => {
-        // Créé la requête pour récupérer l'activité avec toutes ses dépenses associées
-        const getActiviteAvecDepensesQuery = `SELECT a.*, json_agg(d.*) depenses FROM fiche.activite a LEFT JOIN fiche.depense d ON a.id = d.id_activite WHERE a.id=$1 GROUP BY a.id`;
+      // Créé la requête pour récupérer l'activité avec toutes ses dépenses associées
+      const getActiviteAvecDepensesQuery = `SELECT a.*, json_agg(d.*) depenses FROM fiche.activite a LEFT JOIN fiche.depense d ON a.id = d.id_activite WHERE a.id=$1 GROUP BY a.id`;
 
-        // Envoi de la requête
-        dbConn.pool.query(
-          getActiviteAvecDepensesQuery,
-          [id_activite],
-          (error, results) => {
-            if (error) {
-              throw error;
-            }
-
-            // console.log(
-            //   'putActivite 4' + JSON.stringify(results.rows[0], true, 2)
-            // );
-
-            // Envoi de la réponse
-            response.status(200).send(results.rows[0]);
+      // Envoi de la requête
+      dbConn.pool.query(
+        getActiviteAvecDepensesQuery,
+        [id_activite],
+        (error, results) => {
+          if (error) {
+            throw error;
           }
-        );
-      });
+
+          // console.log(
+          //   'putActivite 4' + JSON.stringify(results.rows[0], true, 2)
+          // );
+
+          // Envoi de la réponse
+          response.status(200).send(results.rows[0]);
+        }
+      );
     }
   );
 };
