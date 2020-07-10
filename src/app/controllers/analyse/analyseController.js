a4f80a74901826f90f821f2057193211a1d8fc60
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-07-10 23:50:59 +0200

    simplify get analyse by id

--- src/app/controllers/analyse/analyseController.js
+++ src/app/controllers/analyse/analyseController.js
@@ -53,7 +53,7 @@ const postAnalyse = (request, response) => {
   );
 };
 
-// ---- RECUPERER LES INFORMATIONS D'UNE ANALYSE ---- //
+// ---- RECUPERER LES INFORMATIONS PRINCIPALES D'UNE ANALYSE ---- //
 const getAnalyseById = (request, response) => {
   const id = request.params.id;
 
@@ -83,29 +83,11 @@ const getAnalyseById = (request, response) => {
             f.*,
             ft.libelle libelle_fiche_technique,
             p.libelle libelle_production,
-            p.type_production type_production,
-            json_agg(
-              json_build_object(
-                'id', d.id,
-                'libelle_categorie', d.libelle_categorie,
-                'coeff_intraconsommation', d.coeff_intraconsommation
-              )
-            ) coeff_depenses
-            ,
-            json_agg(
-              json_build_object(
-                'id', v.id,
-                'libelle_categorie', v.libelle_categorie,
-                'coeff_intraconsommation', v.coeff_intraconsommation
-              )
-            ) coeff_ventes
+            p.type_production type_production
            
           FROM 
             analyse_fiche.fiche_technique_libre f
-          LEFT JOIN analyse_fiche.coeff_depense d
-            ON f.id = d.id_fiche_technique_libre
-          LEFT JOIN analyse_fiche.coeff_vente as v
-            ON f.id = v.id_fiche_technique_libre
+
           LEFT JOIN fiche.fiche_technique ft
             ON ft.id = f.id_fiche_technique::integer
           LEFT JOIN fiche.production p
